99bbb4bd6165cb3cd093f4cb7caa17aee6ed7e1a
brameldering <bram.eldering@gmail.com>
2023-06-07 11:41:50 +0200

    Added useState and button to test state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setIsShowParagraph] = useState(false);
+
+  const toggleParagrahHandler = (event) => {
+    event.preventDefault();
+    setIsShowParagraph(!showParagraph);
+  };
+
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
+    <>
+      <div className='app'>
+        <h1>Hi there!</h1>
+        {showParagraph && <p>This is new!</p>}
+      </div>
+      <div className='app'>
+        <Button onClick={toggleParagrahHandler}>Change state!</Button>
+      </div>
+    </>
   );
 }
 
