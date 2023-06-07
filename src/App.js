9d5847a0cb90ba3acb6fb02f4aacfde6bfbe0940
brameldering <bram.eldering@gmail.com>
2023-06-07 18:05:48 +0200

    memo and useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 import "./App.css";
@@ -7,15 +7,23 @@ function App() {
   console.log("App running");
 
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  const toggleParagraphHandler = (event) => {
-    setShowParagraph((prevState) => !prevState);
+  const toggleButtonHandler = () => {
+    setAllowToggle((prevState) => !prevState);
   };
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevState) => !prevState);
+    }
+  }, [allowToggle]);
 
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleButtonHandler}>Allow toggling</Button>
       <Button onClick={toggleParagraphHandler}>Change state!</Button>
     </div>
   );
