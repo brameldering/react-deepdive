12f86802c13bf444cf64f53367f37488da896741
brameldering <bram.eldering@gmail.com>
2023-06-07 12:37:25 +0200

    Showing that child components are always re-evaluated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 import "./App.css";
 
 function App() {
+  console.log("App running");
+
   const [showParagraph, setShowParagraph] = useState(false);
 
   const toggleParagraphHandler = (event) => {
@@ -12,7 +15,7 @@ function App() {
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Change state!</Button>
     </div>
   );
